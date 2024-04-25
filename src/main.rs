765b9dbf0d7a671f2dee6f751f4e85490b1bbf48
Rakarake <rak@rakarake.xyz>
2024-04-26 00:09:18 +0200

    Foxes

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, spawn_gltf)
         .add_systems(Update, sprite_movement)
         .run();
 }
@@ -15,7 +17,7 @@ enum Direction {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera3d::default());
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("ExpressiveStone.png"),
@@ -26,12 +28,59 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));
 }
 
+fn spawn_gltf(
+    mut commands: Commands,
+    ass: Res<AssetServer>,
+) {
+    // note that we have to include the `Scene0` label
+    let my_gltf = ass.load("Fox.gltf#Scene0");
+
+    // to position our 3d model, simply use the Transform
+    // in the SceneBundle
+    commands.spawn(SceneBundle {
+        scene: my_gltf,
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..Default::default()
+    });
+
+    commands.insert_resource(AmbientLight {
+        color: Color::ORANGE_RED,
+        brightness: 0.02,
+    });
+
+    commands.spawn(PointLightBundle {
+            // transform: Transform::from_xyz(5.0, 8.0, 2.0),
+            transform: Transform::from_xyz(-10.0, -5.0, 0.0),
+            point_light: PointLight {
+                intensity: 100_000.0,
+                color: Color::RED,
+                shadows_enabled: true,
+                ..default()
+            },
+            ..default()
+        });
+}
+
+#[derive(Component)]
+struct MyCameraMarker;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(500.0, 520.0, 16.0)
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        MyCameraMarker,
+    ));
+}
+
 /// The sprite is animated by changing its translation depending on the time that has passed since
 /// the last frame.
 fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
     for (mut logo, mut transform) in &mut sprite_position {
         match *logo {
-            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Up => transform.translation.y += 180. * time.delta_seconds(),
             Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
         }
 
