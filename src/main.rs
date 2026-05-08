0025f68180348d54de00df2e8ce3d7331fdd2855
Rakarake <raka@rakarake.xyz>
2026-05-08 20:50:41 +0200

    globular

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use bevy::{
 use std::f32::consts::*;
 use bevy::camera_controller::free_camera;
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
-use bevy_rapier3d::prelude::*;
+use avian3d::prelude::*;
 
 fn main() {
     App::new()
@@ -18,9 +18,9 @@ fn main() {
         // inspector
         .add_plugins(EguiPlugin::default())
         .add_plugins(WorldInspectorPlugin::new())
-        // rapier
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugins(RapierDebugRenderPlugin::default())
+        // avian physics
+        .add_plugins(PhysicsPlugins::default())
+        .add_plugins(avian3d::debug_render::PhysicsDebugPlugin)
 
         .add_systems(Startup, setup)
         //.add_systems(Startup, (
@@ -28,6 +28,7 @@ fn main() {
         //            setup_physics
         //    ).chain()
         //)
+        //.add_systems(Update, setup_physics)
         .add_systems(Update, setup_physics)
         .add_systems(Update, animate_light_direction)
         .run();
@@ -65,49 +66,78 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(SceneRoot(asset_server.load(
         GltfAssetLabel::Scene(0).from_asset("Fox.gltf"),
     )));
-    commands.spawn(SceneRoot(asset_server.load(
-        GltfAssetLabel::Scene(0).from_asset("Platform.glb"),
-    )));
+    commands.spawn(
+        (
+            SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("Platform.glb"),)),
+            ColliderConstructorHierarchy::new(ColliderConstructor::TrimeshFromMesh)
+        )
+    );
+
+    // Some ball or something
+    commands.spawn((
+        //RigidBody::Dynamic,
+        Collider::sphere(0.5),
+        // Overwrite default collider color (optional)
+        DebugRender::default().with_collider_color(Color::srgb(1.0, 0.0, 0.0)),
+    ));
 }
 
-fn setup_physics(
-        mut commands: Commands,
-        meshes: Res<Assets<Mesh>>,
-        q_children: Query<(Entity, &Mesh3d, &ChildOf), Without<Collider>>,
-        q_parents: Query<&Name>,
-    ) {
-    for (entity, mesh, parent) in q_children.iter() {
-        //println!("{name}: has a mesh!!! 🥰🥰🥰");
-        if let Ok(parent_name) = q_parents.get(parent.0) {
-            if parent_name.as_str() == "Suzanne" || parent_name.as_str() == "Ground" {
-                let mesha = meshes.get(&mesh.0);
-                match mesha {
-                    Some(mesha) => {
-                        let mayb_collido = Collider::from_bevy_mesh(mesha, &ComputedColliderShape::TriMesh(TriMeshFlags::FIX_INTERNAL_EDGES));
-                        match mayb_collido {
-                            Some(collido) => {
-                                println!("ADDING COLLIDER 🍓");
-                                commands.entity(entity).insert((
-                                    collido,
-                                ));
-                                if parent_name.as_str() == "Suzanne" {
-                                    commands.entity(entity).insert((
-                                        RigidBody::Dynamic,
-                                        Restitution::coefficient(0.7),
-                                        //Transform::from_xyz(0.0, 4.0, 0.0),
-                                    ));
-                                }
-                            },
-                            None => todo!(),
-                        }
-                    },
-                    None => todo!(),
-                }
+fn setup_physics(mut commands: Commands, query: Query<(Entity, &Name, &ChildOf), Added<Mesh3d>>, parents: Query<&Name>) {
+    for (entity, name, parent) in query {
+        if let Ok(parent_name) = parents.get(parent.0) {
+            match parent_name.as_str() {
+                "Suzanne" => {
+                    commands.entity(entity).insert(RigidBody::Dynamic);
+                },
+                "Ground" => {
+                    commands.entity(entity).insert(RigidBody::Static);
+                    println!("🌱🌱🌱🌱");
+                },
+                _ => ()
             }
-
         }
+        println!("addeddddsd!!!🚴🚴🚴");
     }
 }
+
+//fn setup_physics(
+//        mut commands: Commands,
+//        meshes: Res<Assets<Mesh>>,
+//        q_children: Query<(Entity, &Mesh3d, &ChildOf), Without<Collider>>,
+//        q_parents: Query<&Name>,
+//    ) {
+//    for (entity, mesh, parent) in q_children.iter() {
+//        //println!("{name}: has a mesh!!! 🥰🥰🥰");
+//        if let Ok(parent_name) = q_parents.get(parent.0) {
+//            if parent_name.as_str() == "Suzanne" || parent_name.as_str() == "Ground" {
+//                let mesha = meshes.get(&mesh.0);
+//                match mesha {
+//                    Some(mesha) => {
+//                        //let mayb_collido = Collider::from_bevy_mesh(mesha, &ComputedColliderShape::TriMesh(TriMeshFlags::FIX_INTERNAL_EDGES));
+//                        //match mayb_collido {
+//                        //    Some(collido) => {
+//                        //        println!("ADDING COLLIDER 🍓");
+//                        //        commands.entity(entity).insert((
+//                        //            collido,
+//                        //        ));
+//                        //        if parent_name.as_str() == "Suzanne" {
+//                        //            commands.entity(entity).insert((
+//                        //                RigidBody::Dynamic,
+//                        //                Restitution::coefficient(0.7),
+//                        //                //Transform::from_xyz(0.0, 4.0, 0.0),
+//                        //            ));
+//                        //        }
+//                        //    },
+//                        //    None => todo!(),
+//                        //}
+//                    },
+//                    None => todo!(),
+//                }
+//            }
+//
+//        }
+//    }
+//}
 
 fn animate_light_direction(
     time: Res<Time>,
